a57878d4cb28010114d849630d90eb9f66d9e6be
Jonathan <jmoral4@gmail.com>
2017-10-30 21:04:05 -0400

    Setup Project
    Setup geonbit, console, and core portable

---
+++ RogueSquad.Client.Dx/Game1.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using RogueSquad.Core;
+using GeonBit.UI;
+using GeonBit.UI.Entities;
+using QuakeConsole;
+
+namespace RogueSquad.Client.Dx
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        ConsoleComponent _console;
+   
+
+        
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+ 
+            Window.AllowUserResizing = true;
+           // IsMouseVisible = true;
+            IsFixedTimeStep = false;
+            graphics.SynchronizeWithVerticalRetrace = false;
+            _console = new ConsoleComponent(this);
+            Components.Add(_console);
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            // GeonBit.UI: Init the UI manager using the "hd" built-in theme
+            UserInterface.Initialize(Content, BuiltinThemes.hd);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // create a panel and position in center of screen
+            Panel panel = new Panel(new Vector2(400, 400), PanelSkin.Default, Anchor.Center);
+            UserInterface.Active.AddEntity(panel);
+
+            // add title and text
+            panel.AddChild(new Header("Example Panel"));
+            panel.AddChild(new HorizontalLine());
+            panel.AddChild(new Paragraph("This is a simple panel with a button."));
+
+            // add a button at the bottom
+            panel.AddChild(new Button("Click Me!", ButtonSkin.Default, Anchor.BottomCenter));
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        KeyboardState previousKeyboardState;
+        KeyboardState currentKeyboardState;
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            currentKeyboardState = Keyboard.GetState();
+            // Exit() is obsolete on iOS
+            #if !__IOS__ && !__TVOS__
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            #endif
+            // GeonBit.UIL update UI manager
+            UserInterface.Active.Update(gameTime);
+            // TODO: Add your update logic here
+            // cool.Exec();
+            
+            if (previousKeyboardState.IsKeyUp(Keys.OemTilde) && currentKeyboardState.IsKeyDown(Keys.OemTilde))
+                _console.ToggleOpenClose();
+
+            previousKeyboardState = currentKeyboardState;
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            // GeonBit.UI: draw UI using the spriteBatch you created above
+            UserInterface.Active.Draw(spriteBatch);
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
